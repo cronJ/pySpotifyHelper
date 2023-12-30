b851e326a8847939ff6298cedf38d32e2e6c6b50
cronJ <privat@cronj.de>
2023-12-30 13:36:46 +0100

    Change authentication and add sample playlist code

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+SPOTIFY_USER = os.environ.get("SPOTIFY_USER")
 
 
 def main() -> None:
-    pass
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    playlists = sp.user_playlists(SPOTIFY_USER)
+    while playlists:
+        for i, playlist in enumerate(playlists["items"]):
+            print(
+                "%4d %s %s"
+                % (i + 1 + playlists["offset"], playlist["uri"], playlist["name"])
+            )
+        if playlists["next"]:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
 
 
 if __name__ == "__main__":
