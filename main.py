0860bd729dbb03d75ce94295dd7071f74b7396d1
Alexander Ransmann <privat@cronj.de>
2023-12-30 13:12:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import spotipy
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
